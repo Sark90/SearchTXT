e16da0deed2d6368f5dc26855ffc2f4a2e2f3c5f
Sark90 <mmg9658@gmail.com>
2018-03-21 23:23:32 +0200

    Search txt-files in specified directory

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Main {
+    public static void searchTXT(String path) {
+        File f1 = new File(path);
+        if (f1.isDirectory()) {
+            String s[] = f1.list();
+            for (int i=0; i < s.length; i++) {
+                File f = new File(path + "\\" + s[i]);
+                if (f.isDirectory()) {
+                    searchTXT(f.getAbsolutePath());
+                } else {
+                    if(f.getName().endsWith(".txt")) {
+                        System.out.println(f.getAbsolutePath());
+                    }
+                }
+            }
+        } else {
+            System.out.println(path + " is not a directory");
+        }
+    }
+
+    public static void main(String[] args) {
+        String dirname;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Type the path: ");
+        dirname = scanner.nextLine();
+        System.out.println("txt-files:");
+        /*if(args.length !=0) {
+            dirname = args[0];
+        } else {
+            dirname = "E:\\Books\\Programming";
+        }*/
+        searchTXT(dirname);
+    }
+}
